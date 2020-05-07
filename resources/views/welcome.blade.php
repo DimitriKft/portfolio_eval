c9f1759fe48055dfb183950403d334d24159e7b4
klopfstein <dimitri.kft@gmail.com>
2020-05-07 10:22:07 +0200

    redirect mesg for success to message and categorie

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,11 @@
 @section('content')
 <div class="container">
         <div class="content">
+                @if(session()->get('success'))
+      <div class="alert alert-success">
+        {{ session()->get('success') }}  
+      </div><br />
+    @endif
                 <div class="title m-b-md">
                     Porfolio DK
                 </div>
